715dc71a9ade8c04d9de4ca932d7104009bfd737
morozantona2lab <4.studyingf@gmail.com>
2018-10-01 20:00:29 +0300

    Calculator js code

---
+++ Lecture1/Task1.2/calculator.js
@@ -0,0 +1,74 @@
+var x,y,z;
+function mu(){
+	x=document.getElementById('line').value;
+	document.getElementById('line').value='';
+	//x=parseINT(x);
+	z='*';
+}
+function pl(){
+	x=document.getElementById('line').value;
+	document.getElementById('line').value='';
+	//x=parseINT(x);
+	z='+';
+}
+function mi(){
+	x=document.getElementById('line').value;
+	document.getElementById('line').value='';
+	//x=parseINT(x);
+	z='-';
+}
+function di(){
+	x=document.getElementById('line').value;
+	document.getElementById('line').value='';
+	//x=parseINT(x);
+	z='/';
+}
+function fa(){
+	x=document.getElementById('line').value;
+	document.getElementById('line').value='';
+	//x=parseINT(x);
+	z='!';
+}
+function sqt(){
+	x=document.getElementById('line').value;
+	document.getElementById('line').value='';
+	//x=parseINT(x);
+	z='√';
+}
+function sq(){
+	x=document.getElementById('line').value;
+	document.getElementById('line').value='';
+	//x=parseINT(x);
+	z='^2';
+}
+function eq(){
+	y=document.getElementById('line').value;
+	switch (z){
+		case '*':document.getElementById('res').innerHTML=x*y;
+		break;
+		case '/':document.getElementById('res').innerHTML=x/y;
+		break;
+		case '-':document.getElementById('res').innerHTML=x-y;
+		break;
+		case '+':document.getElementById('res').innerHTML=x-y*(-1);
+		break;
+		case '√':{
+			y=Math.sqrt(x);
+			document.getElementById('res').innerHTML=y;
+		}
+		break;
+		case '^2':document.getElementById('res').innerHTML=x*x;
+		break;
+		case '!':
+			{	
+				var i=0;
+				y=1;
+				while(i<x){
+					i+=1;
+					y=y*x;
+				}
+			document.getElementById('res').innerHTML=y;
+		}
+		break;
+	}
+}
